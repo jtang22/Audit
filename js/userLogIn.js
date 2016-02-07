fec8c434e34b040839660479016cbafc65fc73e9
Jordan Tang <jordantang0229@gmail.com>
2016-02-07 15:16:33 -0800

    Added user token checking, emailing password reset, displaying user information on profile page

--- js/userLogIn.js
+++ js/userLogIn.js
@@ -4,13 +4,8 @@ function loginUser() {
     
     Parse.User.logIn(username, password, {
        success: function(user) {
-           alert("Signed in!");
-            document.getElementById("loginUsername").style.display = 'none';
-            document.getElementById("loginPassword").style.display = 'none';
-            document.getElementById("signInButton").style.display = 'none';
-            document.getElementById("signUpButton").style.display = 'none';
-            document.getElementById("signOutButton").style.display = '';
-            document.getElementById("profileButton").style.display = '';
+            alert("Signed in!");
+            showLogOut();
        }, 
         error: function(user, error) {
             alert("error!" + error.code + " " + error.message);
@@ -22,12 +17,37 @@ function loginUser() {
 function logoutUser() {
     Parse.User.logOut();
     alert("Signed Out!");
+    showLogIn();
+}
+
+function showLogOut() {
+    document.getElementById("loginUsername").style.display = 'none';
+    document.getElementById("loginPassword").style.display = 'none';
+    document.getElementById("signInButton").style.display = 'none';
+    document.getElementById("signUpButton").style.display = 'none';
+    document.getElementById("passwordReset").style.display = 'none';
+    document.getElementById("signOutButton").style.display = '';
+    document.getElementById("profileButton").style.display = '';    
+}
+
+function showLogIn() {
     document.getElementById("loginUsername").style.display = '';
     document.getElementById("loginPassword").style.display = '';
     document.getElementById("signInButton").style.display = '';
     document.getElementById("signUpButton").style.display = '';
+    document.getElementById("passwordReset").style.display = '';
     document.getElementById("signOutButton").style.display = 'none';
-    document.getElementById("profileButton").style.display = 'none';
+    document.getElementById("profileButton").style.display = 'none';    
+}
+
+function checkUserLoggedIn() {
+    var user = Parse.User.current();
+    if(user) {
+        showLogOut();
+    }
+    else {
+        showLogIn();
+    }
 }
 
 function checkEmail(email)
@@ -44,7 +64,7 @@ function checkEmail(email)
 
 function resetPassword()
 {           
-    var email = document.forms["resetpassword"]["email"].value;
+    var email = document.getElementById("email").value;
     var emailIsValid = checkEmail(email);
 
     if (!emailIsValid)
@@ -59,8 +79,50 @@ function resetPassword()
                     return true;
                 },
                 error:function(error) {
-                    window.alert("Could not send reset link to " + email + ". Please try again.");
+                    window.alert("Could not send reset link to " + email + ". Please try again." + error.code + " " + error.message);
                     return false;
                 }
     });
+}
+
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == null || x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
+
+function loadUserData() {
+    var user = Parse.User.current();
+    var name;
+    var numReviews;
+    var reviews;
+    var para;
+    var node;
+    var element = document.getElementById("userInfoDiv");
+    name = user.get("firstname") + " " + user.get("lastname");
+    reviews = user.get("reviews");
+    numReviews = reviews.length;
+
+    para = document.createElement("p");
+    node = document.createTextNode(name);
+    para.appendChild(node);
+    element.appendChild(para);
+    
+    para = document.createElement("p");
+    if(reviews.length == 1) {
+        node = document.createTextNode(numReviews + " Review")
+    }
+    else {
+        node = document.createTextNode(numReviews + " Reviews");
+    }
+    para.appendChild(node);
+    element.appendChild(para);
+    for (var i = 0; i < numReviews; i++) {
+        para = document.createElement("p");
+        node = document.createTextNode(reviews[i]);
+        para.appendChild(node);
+        element.appendChild(para);
+    }
 }
